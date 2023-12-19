6df3f2e7bd6f6bc13b586a39c863e64356552339
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-12-19 11:40:47 -0300

    feat: CRUD users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,7 +4,7 @@ class UserController {
 
   async store(req, res) {
 
-    let {name, email, password} = req.body
+    let { name, email, password } = req.body
 
     try {
       const user = await User.create({
@@ -13,8 +13,76 @@ class UserController {
         password
       })
       res.json(user)
-    } catch(err) {
-      res.status(400).json({msg: err.errors[0].message})
+    } catch (err) {
+      res.status(400).json({ msg: err.errors[0].message })
+      throw new Error(err)
+    }
+  }
+
+  async index(req, res) {
+    try {
+      let users = await User.findAll()
+      res.json(users)
+    } catch (err) {
+      res.status(500).json({ msg: "Internal Server Error" })
+      throw new Error(err)
+    }
+  }
+
+  async show(req, res) {
+    let id = req.params.id
+    try {
+      let user = await User.findByPk(id)
+      res.json(user)
+    } catch (err) {
+      res.status(500).json({ msg: "Internal Server Error" })
+      throw new Error(err)
+    }
+  }
+
+  async update(req, res) {
+    let { name, email, password } = req.body
+    let id = req.params.id
+    try {
+
+      if (!id) {
+        return res.status(400).json({ msg: "ID Missing" })
+      }
+
+      const user = (await User.findByPk(id))
+
+      if (!user) {
+        return res.status(404).json({ msg: "User not found" })
+      }
+
+      const newUser = await user.update({ name, email, password })
+      res.status(200).json(newUser)
+
+    } catch (err) {
+      res.status(400).json({ msg: err.errors[0].message })
+      throw new Error(err)
+    }
+  }
+
+  async delete(req, res) {
+    let id = req.params.id
+    try {
+      if (!id) {
+        return res.status(400).json({ msg: "ID Missing" })
+      }
+
+      let user = await User.findByPk(id)
+
+      if (!user) {
+        return res.status(404).json({ msg: "User not found" })
+      }
+
+      await user.destroy()
+
+      res.status(200).json({msg: "User deleted"})
+
+    } catch (err) {
+      res.status(500).json({ msg: "Internal Server Error" })
       throw new Error(err)
     }
   }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,11 @@ import {Router} from "express";
 import userController from "../controllers/UserController.js"
 const router = Router()
 
+router.get("/", userController.index)
 router.post("/", userController.store)
+router.get("/:id", userController.show)
+router.put("/:id", userController.update)
+router.delete("/:id", userController.delete)
 
 export default router
+
