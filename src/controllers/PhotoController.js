146bf8393e877ccc1a04368779f6a8f834684dc3
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-12-20 10:19:18 -0300

    fix: filter mimetype photos

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,6 +1,16 @@
+import multer from "multer"
+import multerConfig from "../config/multerConfig.js"
+
+const upload = multer(multerConfig).single("photo")
+
 class PhotoController {
   async store(req, res) {
-    res.json("index")
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({ errors: [err.code] })
+      }
+      res.status(200).json(req.file)
+    })
   }
 }
 
--- src/routes/PhotoRoutes.js
+++ src/routes/PhotoRoutes.js
@@ -1,12 +1,8 @@
 import { Router } from "express";
-import multer from "multer";
 import photoController from "../controllers/PhotoController.js";
-import multerConfig from "../config/multerConfig.js"
-
-const upload = multer(multerConfig)
 
 const router = Router()
 
-router.post("/", upload.single("photo"), photoController.store)
+router.post("/", photoController.store)
 
 export default router
